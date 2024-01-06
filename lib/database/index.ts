47eccab9091a2d650d75ae1c994340bb74daa6d5
Abhishek Kumar <ak3037308@gmail.com>
2024-01-06 23:09:27 +0530

    database index modified

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,9 +5,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any ).mongoose || {conn:null, Promise: null };
 
 export const connectToDatabase = async() => {
-    if(cached.conn){
-        return cached.conn;
-    }
+    if(cached.conn) return cached.conn;
+    
     if(!MONGODB_URI) throw new Error('MongoDb uri is missing');
     cached.Promise = cached.Promise ||  mongoose.connect(MONGODB_URI, {
         dbName: 'eventify',
